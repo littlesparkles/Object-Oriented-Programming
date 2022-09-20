df3605fb9c82f292096c67ff7e8595709ec99bc8
littlesparkles <106871734+littlesparkles@users.noreply.github.com>
2022-09-20 19:46:40 +0900

    Add files via upload

---
+++ 20223079-Lab04/src/DoubleTester.java
@@ -0,0 +1,9 @@
+public class DoubleTester 
+{
+	public static void main (String args[])
+	{
+		double probability = 8.70;
+		double percentage = 100 * probability;
+		System.out.println(Math.round(percentage));
+	}
+}
