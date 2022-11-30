ad1f0cea2dfc394c9d6369cff8689aaa6e886378
littlesparkles <106871734+littlesparkles@users.noreply.github.com>
2022-12-01 04:53:06 +0900

    Add files via upload

---
+++ JavaLab10/Lab10/PPoint.java
@@ -0,0 +1,19 @@
+package kr.ac.kookmin.cs;
+
+public class PPoint {
+	int xA;
+	int yA;
+	
+	public PPoint(int x, int y) {
+		xA = x;
+		yA = y;
+	};
+	
+	public int getX() {
+		return xA;
+	}
+	
+	public int getY() {
+		return yA;
+	}
+}
---
+++ JavaLab10/Lab10/PPointTest.java
@@ -0,0 +1,8 @@
+import kr.ac.kookmin.cs.*;
+
+class PPointTest {
+	public static void main(String args[]) {
+		PPoint aObj = new PPoint(10, 20);
+		System.out.println("aObj(x, y) = " + aObj.getX() + ", "+ aObj.getY());
+    }
+}
